fa4a9a83c934b54d796f5c1e29104eaf4fc6e7cf
Octavio Lara <octavioasapchi@gmail.com>
2024-01-30 19:56:37 -0400

    ADD root route

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from src.database import engine, SessionLocal
+
 app = FastAPI()
 
-
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
