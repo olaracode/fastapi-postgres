eae60219e6c1f5cb2f8a8b2a5973510bcacfc441
Octavio Lara <octavioasapchi@gmail.com>
2024-01-31 18:24:22 -0400

    ADD Cors middleware

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.db.database import engine, SessionLocal
 from src.routes.user import router as UserRouter
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(UserRouter, prefix="/user", tags=["user"])
 
 @app.get("/")
